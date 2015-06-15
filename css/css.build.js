141ffaaeb7c108ea9ea3eb1ea3d692e7263b9442
regisrob <regisrobineau@gmail.com>
2015-06-15 21:47:25 +0200

    Change output path of minified css

--- css/css.build.js
+++ css/css.build.js
@@ -1,5 +1,5 @@
 ({
 	cssIn: '../style.css', // Entrée : css à optimiser
-	out: '../build/css/style.min.css', // Sortie : css optimisé
+	out: '../style.min.css', // Sortie : css optimisé
 	optimizeCss: 'standard.keepLines' // Mode d'optimisation (on conserve les sauts de ligne)
 })
